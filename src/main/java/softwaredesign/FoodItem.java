d3798a7a1ae6c1b40642067f7928e14d8b42c3e3
TheBigLebowski123 <124527749+TheBigLebowski123@users.noreply.github.com>
2023-03-31 17:55:17 +0200

    Update FoodItem.java
    cleaned up the code a little

--- src/main/java/softwaredesign/FoodItem.java
+++ src/main/java/softwaredesign/FoodItem.java
@@ -3,15 +3,13 @@ package softwaredesign;
 public class FoodItem {
     private String name;
     private int nutritionPoints;
-    //private int weightPoints;
-    //private int happinessPoints;
+   
     private int healthPoints;
 
     public FoodItem(String name, int nutritionPoints, int healthPoints) {
         this.name = name;
         this.nutritionPoints = nutritionPoints;
-        //this.weightPoints = weightPoints;
-        //this.happinessPoints = happinessPoints;
+       
         this.healthPoints = healthPoints;
     }
 
@@ -23,13 +21,7 @@ public class FoodItem {
         return nutritionPoints;
     }
 
-  /*  public int getWeightPoints() {
-        return weightPoints;
-    }
-
-    public int getHappinessPoints() {
-        return happinessPoints;
-    }*/
+ 
 
    public int getHealthPoints() {
         return healthPoints;
